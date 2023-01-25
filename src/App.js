c39bab60ce82b4c3ddc0274c516e21f00506298c
hillova <83608015+hillova@users.noreply.github.com>
2023-01-25 14:04:39 +0100

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lucie Hillova and is{" "}
+        <a
+          href="https://github.com/hillova/react-weather-app-final"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
