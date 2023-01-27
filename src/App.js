324b5c8dc4806c1942126d9907ac63d4ecfa5c4f
hillova <83608015+hillova@users.noreply.github.com>
2023-01-27 14:05:50 +0100

    added html structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Lucie Hillova and is{" "}
-        <a
-          href="https://github.com/hillova/react-weather-app-final"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Lucie Hillova and is{" "}
+          <a
+            href="https://github.com/hillova/react-weather-app-final"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
